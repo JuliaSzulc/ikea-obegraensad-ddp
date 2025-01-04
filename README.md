6627fb60b02ebcf928cb780b4340fb6cd44215c8
Adam Suhren Gustafsson <adam.suhren@gmail.com>
2025-01-04 20:32:58 +0100

    Added initial DDP test code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ikea-obegraensad-ddp
+
+## Resources
+
+- [Initial inspiration and ESP32 code taken from IKEA OBEGRÄNSAD Hack/Mod](https://github.com/ph1p/ikea-led-obegraensad)
+- [Python code used to interface with DDP devices taken from LedFx](https://github.com/LedFx/LedFx)
+- [Explanation of DDP protocol](http://www.3waylabs.com/ddp/#Source%20and%20Destination%20IDs) 
+- 16x16 pixel frames of the Bad Apple animation mentioned in [this reddit thread](https://www.reddit.com/r/esp8266/comments/gops3i/i_got_bad_apple_running_on_a_16x16_neopixel/) and downloaded from [here](https://files.catbox.moe/yh7kgt.7z)
---
+++ ddp_camera_test.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2 as cv
+ 
+from ddp import DDPDevice
+
+
+device = DDPDevice(destination="192.168.100.101", destination_port=4048)
+
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+ 
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    small = cv.resize(gray, (16, 16))
+
+    blur = cv.GaussianBlur(small, (9,9), sigmaX=0, sigmaY=0) 
+
+    edges = cv.Canny(image=blur, threshold1=50, threshold2=100) 
+
+    #data = cv.resize(edges, (16, 16))
+
+    device.display(edges)
+
+    edges_big = cv.resize(edges, (512, 512), interpolation=cv.INTER_NEAREST)
+
+    # Display the resulting frame
+    cv.imshow("edges", edges)
+    cv.imshow("edges_big", edges_big)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
